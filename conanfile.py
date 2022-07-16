612dbd9d191e9523c652758ae4ebd423bcd86626
Shaul Friedman <sfriedman@speedata.io>
2022-07-16 18:37:47 +0300

    sss

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class ConanTConan(ConanFile):
     # Optional metadata
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
+    url = "<Packaage recipe repository url here, for issues about the package>"
     description = "<Description of ConanT here>"
     topics = ("<Put some tag here>", "<here>", "<and here>")
 
